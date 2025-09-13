6a62d1642ef1419f6e031ea9dd5c7296d1ef11fa
Muhammad Maaz <143191256+muhammadMaaz221@users.noreply.github.com>
2025-09-13 16:43:42 +0500

    Add files via upload

---
+++ javascript/lec 1/script.js
@@ -0,0 +1,13 @@
+alert("Hello world");
+console.log("The code is runing....")
+var a = prompt("Enter your number")
+var isTrue = confirm("are you sure u wont to blast your computer")
+
+if(isTrue){
+console.log("Your computer is blasting")
+}
+else{
+    console.log("Yor computer is safe")
+}
+
+console.log("YOur number is " + a)
---
+++ javascript/lec 2/script.js
@@ -0,0 +1,40 @@
+console.log("Code is running!");
+
+let a = 4;
+let b = 7;
+let c = "Maaz";
+
+console.log(a+b+7);
+console.log(typeof a,typeof b,typeof c);
+
+{
+    let a = 44;
+    console.log(a);
+}
+console.log(a)
+
+// const as1 = 25;  
+// as1= as1+4;      not allowed because as1 its a costant val
+
+let x = "Maaz Bhai";
+let y = 20;
+let z = 33.2;
+const v = true;
+let n = null;
+let j = undefined;
+
+console.log(x,y,z,v,n,j);
+console.log(typeof x,typeof y,typeof z,typeof v,typeof n,typeof j);
+
+
+let o = {
+    "name": "Maaz",
+    "job role": "Hr Depart",
+    "phone number": "03192106398"
+}
+
+console.log(o)
+
+o.gender = "Male"
+
+console.log(o)
